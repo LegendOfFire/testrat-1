e2c25755858c90830d17c56ce10e2ea3e87fc225
LegendOfFire <seu.liangf@gmail.com>
2016-10-27 12:32:48 +0800

    Server run OK
    Server run OK.

--- testrat/settings.py
+++ testrat/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'console.apps.ConsoleCofig'
+    'console.apps.ConsoleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
